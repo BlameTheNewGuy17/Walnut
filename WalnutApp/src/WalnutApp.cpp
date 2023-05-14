1860ec792adcf4837bf56a41435b9eb4c4f60bff
BlameTheNewGuy17 <101384843+BlameTheNewGuy17@users.noreply.github.com>
2023-05-14 00:31:41 -0700

    Added basic image display as per yt video on Walnut.

--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -6,14 +6,26 @@
 class ExampleLayer : public Walnut::Layer
 {
 public:
+	virtual void OnAttach() override
+	{
+		m_Image = std::make_shared<Walnut::Image>("Dad.png");
+	}
+
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Hello");
 		ImGui::Button("Button");
+		ImGui::Text("This is some text");
+
+		ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		// Uncomment to show ImGui Demo Window
+		//ImGui::ShowDemoWindow();
 	}
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
